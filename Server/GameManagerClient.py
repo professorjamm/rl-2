32e008e16f0a7529debfd58a602955f8efa01263
professorjamm <faizaan.siddiqui015@gmail.com>
2024-05-05 14:19:02 -0500

    updated some server stuff

--- Server/GameManagerClient.py
+++ Server/GameManagerClient.py
@@ -9,33 +9,86 @@ PORT = 1234
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
+def keyPressed():
+        print("Key Pressed")
+
 # Receive data from the server
 received_data = sock.recv(1024)
 decoded_data = received_data.decode("utf-8")
 response = json.loads(decoded_data)
 print(response)
 
+team1Score = 0
+team2Score = 0
+isReady = False
+
+while not isReady:
+        # Check if type is "CHECK READY"
+        if response["type"] == "CHECK_READY":
+                #check with the user if they are ready
+                isReady = True
+                response["type"] = "IS_READY"
+                response["payload"] = True
+                sock.sendall(json.dumps(response).encode("utf-8"))
+                break
+
+# Receive data from the server
+received_data = sock.recv(1024)
+decoded_data = received_data.decode("utf-8")
+response = json.loads(decoded_data)
+print(response)
 # Start the timer
-game_time = response["game_time"]
+
+# Set game_time to the number set on the timer
+game_time = response["payload"]["timer"]
 
 print("\nTimer:")
+
+# Send the initial score update to the server
+score_update = {
+        "type": "SCORE_UPDATE",
+        "payload": {"score1": team1Score, "score2": team2Score}
+}
+sock.sendall(json.dumps(score_update).encode("utf-8"))
  
-while game_time>=0:
+while game_time >= 0:
+        if game_time == 0:
+                # Send the final score update to the server
+                final_score_update = {
+                        "type": "GAME_END",
+                        "payload": {"score1": team1Score, "score2": team2Score, "timer": game_time}
+                }
+                sock.sendall(json.dumps(final_score_update).encode("utf-8"))
+                break
+        if response["type"] == "KEY_INPUT":
+                keyPressed(response["payload"])
         mins, secs = divmod(game_time, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
         print(timeformat, end='\r')
         
+        if game_time%7==0:
+                team1Score += 1
+        elif game_time%5==0:
+                team2Score += 1
+        
         # Update the game time in the response JSON
-        response["game_time"] = game_time
+        response["payload"]["timer"] = game_time
         
         # Send the updated response to the server
         sock.sendall(json.dumps(response).encode("utf-8"))
+        
+        # Update the score in the response JSON
+        score_update["payload"]["score1"] = team1Score
+        score_update["payload"]["score2"] = team2Score
+
+        # Send the updated response to the server
+        sock.sendall(json.dumps(score_update).encode("utf-8"))
         
         #print(game_time)
         time.sleep(1)
         game_time -= 1
 
 print("\n\n")
-
+print("FINAL SCORE: Team 1: ", team1Score, " Team 2: ", team2Score) 
 # Close the socket
 sock.close()
--- Server/GameManagerServer.py
+++ Server/GameManagerServer.py
@@ -4,6 +4,8 @@ import json
 HOST = 'localhost'
 PORT = 1234
 
+isReady = False
+
 def networkStuff(json_data):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -17,20 +19,31 @@ def networkStuff(json_data):
 
     # Send the JSON data to the client side
     cs.sendall(json.dumps(json_data).encode("utf-8"))
+    received_data = cs.recv(1024)
+    decoded_data = received_data.decode("utf-8")
+    response = json.loads(decoded_data)
+    if response["payload"]:
+        isReady = True
+        print("Is the user ready? ", isReady)
 
-    while True:
-        received_data = cs.recv(1024)
-        decoded_data = received_data.decode("utf-8")
-        try:
-            response = json.loads(decoded_data)
-        except json.decoder.JSONDecodeError:
-            print("Game Finished (no more info to decode)")
-            break
-        print(decoded_data)
+    if isReady:
+        with open('gameInfo.json', 'r') as file:
+            time_Data = json.load(file)
+        cs.sendall(json.dumps(time_Data).encode("utf-8"))
+        
+        while True:
+            received_data = cs.recv(1024)
+            decoded_data = received_data.decode("utf-8")
+            try:
+                response = json.loads(decoded_data)
+            except json.decoder.JSONDecodeError:
+                print("Game Finished (no more info to decode)")
+                break
+            print(decoded_data)
 
 def main():
     # Read the initial JSON data from info.json
-    with open('gameInfo.json', 'r') as file:
+    with open('ready.json', 'r') as file:
         initial_data = json.load(file)
 
     # Pass the initial JSON data to networkStuff()
